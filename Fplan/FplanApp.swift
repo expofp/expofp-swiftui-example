3446b31ecca651e226a97af0aa4ae3983f59c911
Vladimir <vladimir@expofp.com>
2022-02-15 19:56:02 +0300

    Update FplanKit

--- Fplan/FplanApp.swift
+++ Fplan/FplanApp.swift
@@ -4,38 +4,48 @@ import FplanKit
 @main
 struct FplanApp: App {
     
-    let fplanView: FplanView
+    @State var url: String = "https://wayfinding.expofp.com"
+    @State var selectedBooth: String? = nil
+    @State var route: Route? = nil
+    @State var currentPosition: Point? = nil
     
-    init() {
-        func fplanReadyHandler(){
-            //Some code
-        }
-        
-        func boothSelectionHandler(boothName: String){
-            //Some code
-        }
-        
-        func routeBuildHandler(route: Route){
-            //Some code
-        }
-        
-        fplanView = FplanView("https://developer.expofp.com/examples/autumnfair.html",
-                              fplanReadyHandler: fplanReadyHandler,
-                              boothSelectionHandler: boothSelectionHandler,
-                              routeBuildHandler: routeBuildHandler)
+    func clear() {
+        self.currentPosition = nil
+        self.route = nil
+        self.selectedBooth = ""
     }
     
-    func selectBooth(_ boothName: String) {
-        fplanView.selectBooth(boothName)
+    func selectBooth() {
+        self.currentPosition = nil
+        self.route = nil
+        self.selectedBooth = "1306"
     }
     
-    func selectBooth(_ from: String, _ to: String, _ exceptUnaccessible: Bool) {
-        fplanView.buildRoute(from, to, exceptUnaccessible)
+    func buildRoute() {
+        self.selectedBooth = nil
+        self.currentPosition = Point(x: 2870, y: 1780)
+        self.route = Route(from: "1306", to: "2206", exceptInaccessible: false)
+    }
+    
+    func directionReady(direction: Direction){
     }
     
     var body: some Scene {
         WindowGroup {
-            fplanView
+            ZStack(alignment: .bottom)
+            {
+                FplanView(url, selectedBooth: $selectedBooth, route: route, currentPosition: currentPosition, buildDirectionAction: directionReady)
+                HStack
+                {
+                    Spacer()
+                    Button("Select booth", action: selectBooth)
+                    Spacer()
+                    Button("Build route", action: buildRoute)
+                    Spacer()
+                    Button("Clear", action: clear)
+                    Spacer()
+                }
+            }
         }
     }
 }
